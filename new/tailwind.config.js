cba4b320b3b2e74b73fae676b4c5b97d92ff1b83
abdulkerimosman <114332018+abdulkerimosman@users.noreply.github.com>
2024-09-10 19:34:44 +0300

    Add files via upload

---
+++ new/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+        fontFamily : {
+          saira : ["Saira Extra Condensed", "sans-serif"],
+        },
+        fontSize: {
+          'xxs': '0.4rem', 
+        },
+        backgroundImage: {
+          'pattern' : "url('./img/pattern.png')",
+        },
+        backgroundSize : {
+          'xs' : '12.5%',
+          'small': '25%',
+          'medium' : '50%',
+        }
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar')({ preferredStrategy: 'pseudoelements' }),
+  ],
+}
+
