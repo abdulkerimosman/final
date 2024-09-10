ce14260b7ae2d3c351af405e6a973496423c578b
abdulkerimosman <114332018+abdulkerimosman@users.noreply.github.com>
2024-09-10 20:08:18 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const body = document.querySelector('#body')
 const blur1 = document.querySelector('#blur1')
 const blur2 = document.querySelector('#blur2')
 const blur3 = document.querySelector('#blur3')
+const blur4 = document.querySelector('#blur4')
+const blur5 = document.querySelector('#blur5')
+const blur6 = document.querySelector('#blur6')
+const blur7 = document.querySelector('#blur7')
+const blur8 = document.querySelector('#blur8')
 
 button.addEventListener('click',()=>{
     if (!button.classList.contains('hidden')){
@@ -14,6 +19,11 @@ button.addEventListener('click',()=>{
         blur1.classList.add('blur');
         blur2.classList.add('blur');
         blur3.classList.add('blur');
+        blur4.classList.add('blur');
+        blur5.classList.add('blur');
+        blur6.classList.add('blur');
+        blur7.classList.add('blur');
+        blur8.classList.add('blur');
         body.classList.add('overflow-hidden');
 
 
@@ -29,7 +39,25 @@ x.addEventListener('click',()=>{
         blur1.classList.remove('blur');
         blur2.classList.remove('blur');
         blur3.classList.remove('blur');
+        blur4.classList.remove('blur');
+        blur5.classList.remove('blur');
+        blur6.classList.remove('blur');
+        blur7.classList.remove('blur');
+        blur8.classList.remove('blur');
         body.classList.remove('overflow-hidden');
     }
     
 })
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') { // Check if the ESC key is pressed
+        if (!table.classList.contains('hidden')) {
+            table.classList.add('hidden');
+            button.classList.remove('hidden');
+            blur1.classList.remove('blur');
+            blur2.classList.remove('blur');
+            blur3.classList.remove('blur');
+            body.classList.remove('overflow-hidden');
+        }
+    }
+});
